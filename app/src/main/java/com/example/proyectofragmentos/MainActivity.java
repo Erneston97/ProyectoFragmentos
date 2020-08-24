7d1e4d8641d804641a9a60e7c6b046d07972fe36
Erneston97 <andres300397@gmail.com>
2020-08-24 18:50:20 -0500

    Añadiendo escuchador de eventos al spinner

--- app/src/main/java/com/example/proyectofragmentos/MainActivity.java
+++ app/src/main/java/com/example/proyectofragmentos/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.proyectofragmentos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Spinner mispinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mispinner=findViewById(R.id.mispinner);
+        mispinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this,(String)mispinner.getSelectedItem() , Toast.LENGTH_SHORT).show();
+            }
+
+
+        });
+
     }
+
+
 }
